36d251146ea3de082dd42c37937a8f775d66f7fc
GdanskaProgramistka <emug@vp.pl>
2021-05-06 11:53:17 +0200

    checking if a number is a prime number

--- src/BasicElementsOfJava/Fibonacci.java
+++ src/BasicElementsOfJava/Fibonacci.java
@@ -2,29 +2,28 @@ package BasicElementsOfJava;
 
 import java.util.Scanner;
 
-//public class Fibonacci {
+public class Fibonacci {
 
 
+    public static void main(String[] args) {
 
-//    public static void main(String[] args) {
-//
-//        Scanner in = new Scanner(System.in);
-//        int x = in.nextInt();
-//
-//        int n0 = 0;
-//        int n1 = 1;
-//        int n2 = 1;
-//        int i = 0;
-//
-//        System.out.println(n0);
-//        System.out.println(n1);
-//
-//        for(i=2; n2<x; i++) {
-//            n2=n0+n1;
-//            n0=n1;
-//            n1=n2;
-//            System.out.println(n2);
-//        }
-//
-//    }
-//}
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+
+        int n0 = 0;
+        int n1 = 1;
+        int n2 = 1;
+        int i = 0;
+
+        System.out.println(n0);
+        System.out.println(n1);
+
+
+        for (i = 2; n2 < x; i++) {
+            n2 = n0 + n1;
+            n0 = n1;
+            n1 = n2;
+            System.out.println(n2);
+        }
+    }
+}
